d144ea5dcf7ebb86a33a3286dcd265b3cd52439a
Nahu <nahucorbalan06@gmail.com>
2026-09-15 15:30:15 -0300

    Doble Salto

--- movimiento-3d-y-grayboxing/Scripts/player.gd
+++ movimiento-3d-y-grayboxing/Scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-@export_group ("Camara")
+@export_group("Camara")
 @export_range(0.0, 1.0) var mouse_sensivity := 0.25
 
 @export_group("Movement")
@@ -8,10 +8,12 @@ extends CharacterBody3D
 @export var acceleration := 20.0
 @export var rotation_speed := 12.0
 @export var jump_impulse := 12.0
+@export var max_jumps := 2
 
 var _camera_input_direction := Vector2.ZERO
 var _last_movement_direction := Vector3.BACK
 var _gravity := -30.0
+var _jumps_left := 0 
 
 @onready var _camera_pivot: Node3D = %"Camara Pivot"
 @onready var _camera: Camera3D = %Camera3D
@@ -53,13 +55,16 @@ func _physics_process(delta: float) -> void:
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
 	velocity.y = y_velocity + _gravity * delta
 	
-	var is_starting_jump := Input.is_action_just_pressed("Saltar") and is_on_floor()
-	if is_starting_jump:
-		velocity.y += jump_impulse
+
+	if is_on_floor():
+		_jumps_left = max_jumps
+	if Input.is_action_just_pressed("Saltar") and _jumps_left > 0:
+		velocity.y = jump_impulse 
+		_jumps_left -= 1
 	
 	move_and_slide()
 	
 	if move_direction.length() > 0.2:
 		_last_movement_direction = move_direction
-	var target_angle := Vector3.BACK .signed_angle_to(_last_movement_direction, Vector3.UP)
+	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
 	_skin.global_rotation.y = lerp_angle(_skin.rotation.y, target_angle, rotation_speed * delta)
